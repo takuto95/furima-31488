e40c8b1ddd0b80168f3ddcc94766c743567a1096
takuto95 <kaza.solution95@gmail.com>
2020-12-12 14:33:46 +0900

    商品登録の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,12 +4,20 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new(item_params)
+    @item = Item.new
   end
 
-private
-def item_params
-  prams.require(:item).permit(:name,:detail,:item_price,:image).merge(user_id: current_user.id)
-end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+  def item_params
+    params.require(:item).permit(:image,:name,:detail,:item_price,:category_id,:sales_status_id,:shipping_fee_status_id,:prefecture_id,:scheduled_delivery_id).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,31 @@
 class Item < ApplicationRecord
+extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to :category
+
 belongs_to :user
 has_one_attached :image
+belongs_to :sales_status
+belongs_to :shipping_fee_status
+belongs_to :prefecture
+belongs_to :scheduled_delivery
+
+with_options presence:true do
+validates :image
+validates :name
+validates :detail
+validates :item_price, numericality: {only_integer: true, message: "is invalid. Input half-width characters."}
+validates :category_id
+validates :sales_status_id
+validates :shipping_fee_status_id
+validates :prefecture_id
+validates :scheduled_delivery_id
+end
+
+validates :category_id, numericality: { other_than: 1 }
+validates :sales_status_id, numericality: { other_than: 1}
+validates :shipping_fee_status_id, numericality: { other_than: 1 }
+validates :prefecture_id, numericality: { other_than: 1 }
+validates :scheduled_delivery_id, numericality: { other_than: 1 }
+validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+
 end
