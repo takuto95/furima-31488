86f143cf97d59a6a2424ce0849a63e27eb5f90e1
takuto95 <kaza.solution95@gmail.com>
2020-12-25 23:03:46 +0900

    商品購入機能単体テスト

--- spec/factories/buyers.rb
+++ spec/factories/buyers.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :buyer do
-    
+    association :item
+    association :user
   end
 end
---
+++ spec/factories/order_buyers.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_buyer do
+    prefecture_id {2}
+    postal_code {"123-4567"}
+    city {"横浜市"}
+    house_number {"青山"}
+    building_name {"１丁目"}
+    phone_number {"09012345678"}
+    token {"tok_abcdefghijk00000000000000000"}
+  end
+end
