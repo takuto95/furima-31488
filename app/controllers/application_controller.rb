774a9721a7c0b2250397c0bd6ff57e8ef558b3a6
takuto95 <kaza.solution95@gmail.com>
2020-12-26 21:51:20 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
 
   private
+
   def basic_auth
     authenticate_or_request_with_thhp_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 
