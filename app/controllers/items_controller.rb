d4baeedb1ea8a211b330854b43383ed6c4079079
takuto95 <kaza.solution95@gmail.com>
2020-12-26 21:21:48 +0900

    修正６

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_index, except: [:index, :show, :new, :create]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -52,8 +53,7 @@ class ItemsController < ApplicationController
   end
 
   def move_index
-    item = Item.find(params[:id])
-    redirect_to root_path if item.buyer.present?
-    redirect_to action: :index unless user_signed_in? && current_user.id == item.user.id
+    redirect_to root_path if @item.buyer.present?
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
   end
 end
